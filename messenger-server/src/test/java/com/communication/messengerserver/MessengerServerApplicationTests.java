b1db5db16d8428544b8b46408b81f13d4a62391c
ArtemFedorov2004 <artem20.fedorov00@mail.ru>
2025-02-07 09:06:24 +0300

    initial commit

---
+++ messenger-server/src/test/java/com/communication/messengerserver/MessengerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.communication.messengerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
