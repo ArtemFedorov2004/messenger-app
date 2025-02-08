6c8b6bcbb3949422def962ded6279c2b81007f2a
ArtemFedorov2004 <artem20.fedorov00@mail.ru>
2025-02-08 07:34:44 +0300

    feat: Implement message sending with persistence in database

---
+++ messenger-server/src/main/java/com/communication/messengerserver/message/MessageController.java
@@ -0,0 +1,31 @@
+package com.communication.messengerserver.message;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/messages")
+public class MessageController {
+
+    private final MessageService messageService;
+
+    @PostMapping
+    public ResponseEntity<Message> sendMessage(@RequestBody Message message) {
+        Message savedMessage = messageService.sendMessage(message);
+        return ResponseEntity.created(
+                        UriComponentsBuilder.fromPath("/messages/{id}")
+                                .buildAndExpand(savedMessage.getId())
+                                .toUri())
+                .body(savedMessage);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Message>> getAllMessages() {
+        return ResponseEntity.ok(messageService.getAllMessages());
+    }
+}
