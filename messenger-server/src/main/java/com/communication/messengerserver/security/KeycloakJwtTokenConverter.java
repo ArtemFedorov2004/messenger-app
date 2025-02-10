0d8afca54268e3b4575facc8184e6385b5a71e9e
ArtemFedorov2004 <artem20.fedorov00@mail.ru>
2025-02-10 18:14:14 +0300

    Set up Keycloak authentication in Spring Boot application

---
+++ messenger-server/src/main/java/com/communication/messengerserver/security/KeycloakJwtTokenConverter.java
@@ -0,0 +1,40 @@
+package com.communication.messengerserver.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RequiredArgsConstructor
+public class KeycloakJwtTokenConverter implements Converter<Jwt, JwtAuthenticationToken> {
+
+    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter;
+
+    @Override
+    public JwtAuthenticationToken convert(Jwt jwt) {
+        var resourceAccess = new HashMap<>(jwt.getClaim("resource_access"));
+
+        var account = (Map<String, List<String>>) resourceAccess.get("account");
+
+        List<String> roles = account.get("roles");
+
+        Stream<SimpleGrantedAuthority> accesses = roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.replace("-", "_")));
+
+        Set<GrantedAuthority> authorities = Stream
+                .concat(jwtGrantedAuthoritiesConverter.convert(jwt).stream(), accesses)
+                .collect(Collectors.toSet());
+
+        return new JwtAuthenticationToken(jwt, authorities);
+    }
+}
---
+++ messenger-server/src/main/java/com/communication/messengerserver/security/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.communication.messengerserver.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final KeycloakJwtTokenConverter keycloakJwtTokenConverter;
+
+    @Value("${allowed-origins}")
+    private List<String> allowedOrigins;
+
+    public SecurityConfig() {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter
+                = new JwtGrantedAuthoritiesConverter();
+
+        this.keycloakJwtTokenConverter =
+                new KeycloakJwtTokenConverter(jwtGrantedAuthoritiesConverter);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(withDefaults())
+                .authorizeHttpRequests(customizer -> customizer.anyRequest().authenticated())
+                .oauth2ResourceServer(customizer -> customizer
+                        .jwt(jwtCustomizer -> jwtCustomizer.jwtAuthenticationConverter(keycloakJwtTokenConverter)))
+                .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(allowedOrigins);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
