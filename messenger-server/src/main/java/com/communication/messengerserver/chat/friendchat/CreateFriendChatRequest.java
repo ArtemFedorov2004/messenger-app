0dcb6cb84e49a6c357fcfde90490b4b0d045ee25
ArtemFedorov2004 <artem20.fedorov00@mail.ru>
2025-03-22 08:48:58 +0300

    delete friend chat

--- messenger-server/src/main/java/com/communication/messengerserver/chat/friendchat/CreateFriendChatRequest.java
+++
@@ -1,8 +0,0 @@
-package com.communication.messengerserver.chat.friendchat;
-
-import com.communication.messengerserver.chat.friendmessage.FriendMessage;
-
-
-public record CreateFriendChatRequest(String friendId, FriendMessage message) {
-}
-
