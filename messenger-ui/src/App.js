1ab45566162c03186b86ac88fe39f604974a8ff4
ArtemFedorov2004 <artem20.fedorov00@mail.ru>
2025-03-27 18:25:57 +0300

    create new react project

--- messenger-ui/src/App.js
+++ messenger-ui/src/App.js
@@ -1,24 +1,10 @@
-import React from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import ChatPage from "./pages/chat/ChatPage";
-import {useUser} from "./contexts/UserContext";
-import ProfilePage from "./pages/profile/ProfilePage";
 
 
 function App() {
-    const {user} = useUser();
-
-    if (user == null) {
-        return null;
-    }
-
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route exact path="/" element={<ChatPage/>}/>
-                <Route path="/me" element={<ProfilePage/>}/>
-            </Routes>
-        </BrowserRouter>
+        <div>
+            Работает!
+        </div>
     );
 }
 
