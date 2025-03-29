92e692898f99346f4ac7b38f5bdbbc54cfd8c4b0
ArtemFedorov2004 <artem20.fedorov00@mail.ru>
2025-03-29 10:54:51 +0300

    implemented mock registration

--- messenger-ui/src/components/AppRouter.jsx
+++ messenger-ui/src/components/AppRouter.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Navigate, Route, Routes} from "react-router-dom";
-import {publicRoutes} from "../routes";
-import {LOGIN_ROUTE} from "../utils/consts";
+import {privateRoutes, publicRoutes} from "../routes";
+import {HOME_ROUTE, LOGIN_ROUTE} from "../utils/consts";
+import {observer} from "mobx-react-lite";
+import {Context} from "../index";
 
-const AppRouter = () => {
+const AppRouter = observer(() => {
+    const {user} = useContext(Context);
+
     return (
-        <Routes>
-            {publicRoutes.map(({path, Component}) =>
-                <Route key={path} path={path} element={Component} exact/>
-            )}
-            <Route
-                path="*"
-                element={<Navigate to={LOGIN_ROUTE} replace/>}
-            />
-        </Routes>
+        user.isAuth
+            ?
+            <Routes>
+                {privateRoutes.map(({path, Component}) =>
+                    <Route key={path} path={path} element={Component} exact/>
+                )}
+                <Route
+                    path="*"
+                    element={<Navigate to={HOME_ROUTE} replace/>}
+                />
+            </Routes>
+            :
+            <Routes>
+                {publicRoutes.map(({path, Component}) =>
+                    <Route key={path} path={path} element={Component} exact/>
+                )}
+                <Route
+                    path="*"
+                    element={<Navigate to={LOGIN_ROUTE} replace/>}
+                />
+            </Routes>
     );
-};
+});
 
 export default AppRouter;
