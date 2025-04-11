a29f4085e3553a531b72ca067dd78635fe97d373
ArtemFedorov2004 <artem20.fedorov00@mail.ru>
2025-04-11 15:50:52 +0300

    DELETE ME

--- messenger-server/src/test/java/com/communication/messengerserver/MessengerServerApplicationTests.java
+++ messenger-server/src/test/java/com/communication/messengerserver/MessengerServerApplicationTests.java
@@ -1,8 +1,11 @@
 package com.communication.messengerserver;
 
+import com.communication.messengerserver.config.TestingBeans;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import(TestingBeans.class)
 @SpringBootTest
 class MessengerServerApplicationTests {
 
--- messenger-server/src/test/java/com/communication/messengerserver/controller/HelloControllerIT.java
+++ messenger-server/src/test/java/com/communication/messengerserver/controller/HelloControllerIT.java
@@ -1,13 +1,18 @@
 package com.communication.messengerserver.controller;
 
+import com.communication.messengerserver.config.TestingBeans;
 import com.communication.messengerserver.entity.User;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Slf4j
+@Import(TestingBeans.class)
 @SpringBootTest
 class HelloControllerIT {
 
@@ -22,5 +27,7 @@ class HelloControllerIT {
         User ret = mongoTemplate.findById(saved.getId(), User.class);
 
         assertEquals(ret.getUsername(), "Artem");
+        log.info(ret.getUsername() + ret.getId());
+        throw new RuntimeException();
     }
 }
